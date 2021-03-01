2c5c1c68aa703b471dc8f7c96218bacf2413ccd0
Eric Chen <eric.chen@f5.com>
2021-03-01 15:21:05 -0500

    no longer hardcode ip

--- terraform/jumphost.tf
+++ terraform/jumphost.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "f5-jumphost-1" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.f5-volterra-management-1.id
-  private_ip             = "10.0.0.4"
+#  private_ip             = "10.0.0.4"
   vpc_security_group_ids = [aws_security_group.volterra-vpc.id]
   key_name               = var.ssh_key
   user_data              = <<-EOF
