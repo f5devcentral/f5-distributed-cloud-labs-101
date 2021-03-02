d62497f68bbfaf1f71925307f2b1162de515b497
Eric Chen <eric.chen@f5.com>
2021-03-02 06:43:59 -0500

    output AWS Console URL

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -16,3 +16,9 @@ output INTERNAL2_SUBNET_ID {
 output AWS_INSTANCE {
   value = aws_instance.f5-jumphost-1.public_ip
 }
+
+data "aws_caller_identity" "current" {}
+
+output AWS_CONSOLE {
+   value = "https://${data.aws_caller_identity.current.account_id}.signin.aws.amazon.com/console?region=${var.aws_region}"
+}
