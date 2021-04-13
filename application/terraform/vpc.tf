e3df365d8147862a62b8470481e35a3112892e83
Eric Chen <eric.chen@f5.com>
2021-04-13 09:40:46 -0400

    expand ACL

--- application/terraform/vpc.tf
+++ application/terraform/vpc.tf
@@ -133,7 +133,7 @@ resource "aws_security_group" "volterra-vpc" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/14"]
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   ingress {
