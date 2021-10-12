50eb26c9de7b33b0282e1c7c9cf551cd35de4f69
Eric Chen <eric.chen@f5.com>
2021-10-12 06:47:20 -0400

    move s3 to separate terraform directory

--- networking/s3/s3.tf
+++ networking/s3/s3.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 resource "aws_s3_bucket" "tf_s3_bucket" {
   bucket_prefix = "${var.prefix}-s3bucket"
 }
@@ -8,18 +12,19 @@ resource "aws_s3_bucket_object" "object" {
   content_base64 = "dGhpcy1pcy1hLXNlY3JldAo="
   acl            = "public-read"
 }
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id          = aws_vpc.f5-volterra-vpc.id
-  service_name    = "com.amazonaws.${var.aws_region}.s3"
-  route_table_ids = [aws_route_table.f5-volterra-vpc-external-rt.id]
-}
+
+# resource "aws_vpc_endpoint" "s3" {
+#   vpc_id          = var.vpc_id
+#   service_name    = "com.amazonaws.${var.aws_region}.s3"
+#   route_table_ids = [var.route_id]
+# }
 
 resource "aws_vpc_endpoint" "interface-s3" {
-  vpc_id             = aws_vpc.f5-volterra-vpc.id
+  vpc_id             = var.vpc_id
   service_name       = "com.amazonaws.${var.aws_region}.s3"
   vpc_endpoint_type  = "Interface"
-  subnet_ids         = [aws_subnet.f5-volterra-internal-1.id]
-  security_group_ids = [aws_security_group.volterra-vpc.id]
+  subnet_ids         = [var.subnet_id]
+  security_group_ids = [var.security_group]
 }
 
 
